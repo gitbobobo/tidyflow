9367de85d6eef2a8593c47dda4c83f8488b44bc5
gitbobobo <godbobo@aliyun.com>
2026-02-06 16:41:26 +0800

    feat(editor): 新增 IDEA、Android Studio、Xcode 和 DevEco Studio 外部编辑器支持

--- app/TidyFlow/Views/Models.swift
+++ app/TidyFlow/Views/Models.swift
@@ -22,12 +22,20 @@ enum ExternalEditor: String, CaseIterable {
     case vscode = "VSCode"
     case cursor = "Cursor"
     case trae = "Trae"
+    case idea = "IDEA"
+    case androidStudio = "Android Studio"
+    case xcode = "Xcode"
+    case devecoStudio = "DevEco Studio"
 
     var bundleId: String {
         switch self {
         case .vscode: return "com.microsoft.VSCode"
         case .cursor: return "com.todesktop.230313mzl4w4u92"
         case .trae: return "com.trae.app"
+        case .idea: return "com.jetbrains.intellij"
+        case .androidStudio: return "com.google.android.studio"
+        case .xcode: return "com.apple.dt.Xcode"
+        case .devecoStudio: return "com.huawei.devecostudio.ds"
         }
     }
 
@@ -36,6 +44,10 @@ enum ExternalEditor: String, CaseIterable {
         case .vscode: return "vscode-icon"
         case .cursor: return "cursor-icon"
         case .trae: return "trae-icon"
+        case .idea: return "idea-icon"
+        case .androidStudio: return "android-studio-icon"
+        case .xcode: return "xcode-icon"
+        case .devecoStudio: return "deveco-studio-icon"
         }
     }
 
@@ -44,6 +56,10 @@ enum ExternalEditor: String, CaseIterable {
         case .vscode: return "chevron.left.forwardslash.chevron.right"
         case .cursor: return "cursorarrow.rays"
         case .trae: return "sparkles"
+        case .idea: return "lightbulb"
+        case .androidStudio: return "apps.iphone"
+        case .xcode: return "hammer"
+        case .devecoStudio: return "star"
         }
     }
 
