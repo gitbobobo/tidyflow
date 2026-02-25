a34eafd63464fbd59549507c927a2a4ff9e4ffb1
gitbobobo <godbobo@aliyun.com>
2026-02-25 10:09:57 +0800

    perf(Evolution): 将最大阶段运行时长从10分钟调整为1小时

--- core/src/server/handlers/evolution/consts.rs
+++ core/src/server/handlers/evolution/consts.rs
@@ -7,6 +7,6 @@ pub(super) const STAGES: [&str; 6] = [
     "report",
 ];
 
-pub(super) const MAX_STAGE_RUNTIME_SECS: u64 = 600;
+pub(super) const MAX_STAGE_RUNTIME_SECS: u64 = 3600;
 pub(super) const DEFAULT_VERIFY_LIMIT: u32 = 3;
 pub(super) const DEFAULT_MAX_PARALLEL: u32 = 4;
